da7583c6875f15356fa722c140fca84b493afcd4
Jonathan Andrade <1140117@unav.edu.mx>
2019-06-12 14:35:08 -0700

    Modify the use of state

--- src/App.js
+++ src/App.js
@@ -2,46 +2,44 @@ import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const app = () => {
-  useState({
+const App = props => {
+  const [personState, setPersonsState] = useState({
     persons: [
-      { name: 'libre', age: '20' },
-      { name: 'pn', age: '20' },
-      { name: 'ud', age: '20' },
-      { name: 'pinu', age: '20' }
-    ]
+      { name: 'jonathan', age: "21" },
+      { name: 'Hola', age: "22" },
+      { name: 'Otro', age: "23" },
+      { name: 'que pedo mae', age: "23" }
+    ],
+    otherState: 'some other value'
   });
+
+  console.log(personState);
+  
+  const switchNameHandler = () => {
+    console.log('this');
+    // do not do this  this.state.persons[0].name = 'aa perrro';
+    setPersonsState({
+      persons: [
+        { name: 'libre', age: '20' },
+        { name: 'pn', age: '20' },
+        { name: 'ud', age: '20' },
+        { name: 'pinu', age: '20' }
+      ]
+    });
+  }
 
   return (
     <div className="App">
       <h1> Hola test</h1>
-      <button onClick={this.handler}>Switch Name</button>
-      <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-      <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-      <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-      <Person name={this.state.persons[3].name} age={this.state.persons[3].age}> My Hobbies: Racing</Person>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={personState.persons[0].name} age={personState.persons[0].age} />
+      <Person name={personState.persons[1].name} age={personState.persons[1].age} />
+      <Person name={personState.persons[2].name} age={personState.persons[2].age} />
+      <Person name={personState.persons[3].name} age={personState.persons[3].age}> My Hobbies: Racing</Person>
     </div>
   );
 
   //   return React.createElement('div', {className: 'App'}, React.createElement('h1', null, ' que ondas como estas?'));
 }
 
-export default app;
-
-state = {
-  persons: [
-    { name: 'jonathan', age: "21" },
-    { name: 'Hola', age: "22" },
-    { name: 'Otro', age: "23" },
-    { name: 'que pedo mae', age: "23" }
-  ],
-  otherState: 'some other value'
-}
-
-handler = () => {
-  console.log('this');
-  // do not do this  this.state.persons[0].name = 'aa perrro';
-
-}
-
-
+export default App;
